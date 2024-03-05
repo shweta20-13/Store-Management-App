a78d1a12f0b0c66de525bf3f93e1fd5398557813
sharmajiiprince <prince.raj.cs19@ggits.net>
2024-03-05 18:05:45 +0530

    ok done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server"); 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middleware = jsonServer.defaults();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require('cors');
 const port = process.env.PORT || 3030; 
 
-server.use(middleware);
+server.use(cors());
+server.use(middlewares);
 server.use(router);
+
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
